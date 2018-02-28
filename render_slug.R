048020e0205bb7a5db4023046038a7b200fcff9a
Augustina Ragwitz <augustina.ragwitz@ibm.com>
2018-02-27 23:00:40 -0800

    Automagicate generating blog entry html

--- render_slug.R
+++ render_slug.R
@@ -1,7 +1,22 @@
 #!/usr/bin/env Rscript
 
-render_slug <- function(slug) {
-  rmarkdown::render("site/blog/countering-bean-counting-commits/countering-bean-counting-commits.Rmd",
-                    output_file="index.html",
-                    output_options = c(self_contained=FALSE))
-}
+slug <- commandArgs()
+path <- paste0("site/blog/", slug, "/")
+
+# render blog post
+rmarkdown::render(paste0(path, slug, ".Rmd"),
+  output_file="index.html",
+  output_options = c(self_contained=FALSE))
+
+# add nav html to blog post
+
+blog_html <- readr::read_lines(paste0(path, "index.html"))
+blog_nav_html <- readr::read_lines("blog_nav.html")
+
+blog_html_nav <- stringr::str_replace(blog_html, "<div class=\"container-fluid main-container\">", 
+                                      paste(blog_nav_html, collapse=""))
+# run the full site rendering
+readr::write_lines(blog_html_nav, paste0(path, "index.html"))
+
+setwd("site")
+rmarkdown::render_site()
