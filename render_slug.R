37d90ab04e3dd524afd587336dd226c7e78bf9e6
Augustina Ragwitz <augustina.ragwitz@ibm.com>
2018-02-28 21:04:51 -0800

    Extract navigation for blog entries automagically, use here()

--- render_slug.R
+++ render_slug.R
@@ -1,23 +1,33 @@
 #!/usr/bin/env Rscript
 
+library(here)
+library(readr)
+library(rmarkdown)
+library(stringr)
+
 slug <- commandArgs()
-path <- paste0("site/blog/", slug, "/")
 
 # render blog post
-rmarkdown::render(paste0(path, slug, ".Rmd"),
-  output_file="index.html",
+render(paste0(slug, ".Rmd"),
+  output_file=here("site", "blog", slug, "index.html"),
   output_options = c(self_contained=FALSE))
 
+render_site(here("site/blog.Rmd"))
+
 # add nav html to blog post
+blog_slug_html <- read_lines(here("site", "blog", slug, "index.html"))
 
-blog_html <- readr::read_lines(paste0(path, "index.html"))
-blog_nav_html <- readr::read_lines("blog_nav.html")
+# extract nav
+blog_html <- read_lines(here("site", "site", "blog.html"))
+blog_html_nav <- 
+  str_extract(paste(blog_html, collapse=""), "<div class=\"navbar navbar-default  navbar-fixed-top\" role=\"navigation\">.*</div><!--/.navbar -->")
 
-blog_html_nav <- stringr::str_replace(blog_html, "<div class=\"container-fluid main-container\">", 
-                                      paste(blog_nav_html, collapse=""))
+blog_slug_nav <- str_replace(blog_slug_html, "<div class=\"container-fluid main-container\">",
+                             paste0("<div class=\"container-fluid main-container\">", 
+                                    blog_html_nav, 
+                                    "<div class=\"fluid-row\"><br /><br /></div>"))
+
 # run the full site rendering
-readr::write_lines(blog_html_nav, paste0(path, "index.html"))
+write_lines(blog_slug_nav, "index.html")
 
-setwd("site")
-rmarkdown::render_site()
-
+render_site(input=here("site"))
